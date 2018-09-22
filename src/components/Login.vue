071b7e19bd33b55113b2a241bd2645b3f6b3e3b3
mehdi-aouadi <mehdi.aouadi@gmail.com>
2018-09-22 22:43:51 +0200

    [38][FRONT] Add element-ui

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  div(class="login")
+    el-row(:span=12 class="center")
+      el-col(:span=24)
+          el-input(placeholder="Login" v-model="login" clearable)
+    el-row(:span=12 class="center")
+      el-col(:span=24)
+        el-input(placeholder="password" type="password" clearable)
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      login: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .center {
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+}
+</style>
