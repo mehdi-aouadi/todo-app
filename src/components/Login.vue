8a92a33caa2a718e8ef6363c204e98a051ec669e
mehdi-aouadi <mehdi.aouadi@gmail.com>
2018-09-23 23:15:21 +0200

    [36][ESLINT] Configure Eslint

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,17 +6,33 @@
     el-row(:span=12 class="center")
       el-col(:span=24)
         el-input(placeholder="password" type="password" clearable)
+    el-row(:span=12 class="center")
+      el-col(:span=24)
+        el-button(type="primary"  @click="startHacking" style="width: 100px;") Login
+    el-row(:span=12 class="center")
+      el-col(:span=24)
+        el-button(type="primary" style="width: 100px;") New User
 </template>
 
 <script>
 export default {
   name: 'Login',
-  data() {
+  data () {
     return {
-      login: '',
-    };
+      login: ''
+    }
   },
-};
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
